4c49fc0e1afccdccef9636cac9208f17b1bf5111
Carlos Varela <cvarela@nevada.unr.edu>
2018-10-17 11:25:28 -0700

    Added 'b' to position 2

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ int main()
 {
   //SL_PriorityQueue<int> q;
   //q.enqueue(1);
-
   std::shared_ptr<LinkedSortedList<char> > names(new LinkedSortedList<char>());
   names->insertSorted('a');
   std::cout << names->getPosition('a') << std::endl;
+  names->insertSorted('b');
+  std::cout << names->getPosition('b') << std::endl;
 }
